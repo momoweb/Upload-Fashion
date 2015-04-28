0af7863897a9f9c69d39b4e0895114bf2eb66236
Yee Wong <ywonggit@gmail.com>
2015-04-28 01:07:52 +0100

    fixed mobile browser triggering resize events when scrolling which causes the REFINE view to collapse

--- src/app/sidebar/rgWatchSize.directive.js
+++ src/app/sidebar/rgWatchSize.directive.js
@@ -10,10 +10,20 @@
         return {
             restrict: 'A',
             link: function(scope) {
+                var oldWidth = $window.innerWidth;
                 angular.element($window).on('resize', function() {
                     scope.$apply(function() {
+                        var newWidth = $window.innerWidth;
+                        /*
+                         * mobile browser can trigger a resize event when you scroll because the address bar
+                         * will change the height of the browser.
+                         * We need to make sure that we are dealing with a browser width change
+                         */
+                        if (newWidth === oldWidth) {
+                            return;
+                        }
                         //if window width is >= desktop
-                        if ($window.innerWidth >= 992) {
+                        if (newWidth >= 992) {
                             scope.vm.isCollapsed = false;
                         } else {
                             scope.vm.isCollapsed = true;
@@ -24,3 +34,37 @@
         };
     }
 }());
+
+//
+///*
+// * sets isCollapse value on resize events
+// */
+//(function() {
+//    'use strict';
+//    angular.module('upload')
+//        .directive('rgWatchResize', rgWatchResize);
+//
+//    function rgWatchResize($window) {
+//        return {
+//            restrict: 'A',
+//            link: function(scope) {
+//                scope.$watch(function(){
+//                    return $window.innerWidth;
+//                }, function(value, oldVal) {
+//                    console.log(value);
+//                    console.log(oldVal);
+//                });
+//                //angular.element($window.innerWidth).on('resize', function() {
+//                //    scope.$apply(function() {
+//                //        //if window width is >= desktop
+//                //        if ($window.innerWidth >= 992) {
+//                //            scope.vm.isCollapsed = false;
+//                //        } else {
+//                //            scope.vm.isCollapsed = true;
+//                //        }
+//                //    });
+//                //});
+//            }
+//        };
+//    }
+//}());
