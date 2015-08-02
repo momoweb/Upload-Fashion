3f9eecc7898c2678744f8549e691d5007f1399ad
Yee Wong <ywonggit@gmail.com>
2015-08-02 19:00:09 +0100

    Refactored folder structure

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
-Gulp tasks:
+[www.busdue.com](http://www.busdue.com/ "Title")  
+A London bus arrival time app built with Angular Material and MEAN stack.
 
-<b>gulp serve-dev</b>
-<br/>
-serves development version on port 8888
+##Features 
++ Search bus via bus number.
++ See arrival times for bus stops
++ Result filtering
++ Save a stop to favourites
+
+###Version 1.0.0
++ Now uses the new TFL Unified API
++ Added Node/Mongodb Backend
+
+###Version 0.9.5
++ Added UI
++ bug fixes
+
+##Installation
+
+Clone the app
 
 
-<b>gulp optimize</b>
-<br/>
-builds dist version
+`$ npm install`  
+To install the dependencies, this will also run `bower install`
+
+`$ gulp serve-dev`  
+Start the webserver  
+
+##Screenshots
+<img src="http://www.ywong.co.uk/images/screenshot.jpg" alt="ngBus app screenshots">  
+[www.busdue.com](http://www.busdue.com/ "Title")  
--- gulp.config.js
+++ gulp.config.js
@@ -1,45 +1,129 @@
 module.exports = function() {
-    var app = './src/';
+    var client = './src/client/';
+    var server = './src/server/';
+    var clientApp = client + 'app/';
+    var report = './report/';
+    var root = './';
+    var specRunnerFile = 'specs.html';
+    var temp = './.tmp/';
+    var wiredep = require('wiredep');
+    var bowerFiles = wiredep({devDependencies: true})['js'];
     var bower = {
-        //json: require('./bower.json'),
-        //directory: './bower_components/',
-        exclude: ['bootstrap.js', 'bootstrap.css'],
-        ignorePath: ['../bower_components/']
+        json: require('./bower.json'),
+        directory: './bower_components/',
+        ignorePath: '../..'
     };
-    var temp = '.tmp/';
+    var nodeModules = 'node_modules';
 
     var config = {
         /**
          * File paths
          */
-        allhtml: app + '**/*.html',
-        // all javascript we want to vet
+        // all javascript that we want to vet
         alljs: [
-            app + 'app/**/*.js'
+            './src/**/*.js',
+            './*.js'
         ],
-        bower: bower,
         build: './build/',
-        fonts: './bower_components/bootstrap/fonts/**.*',
-        images: './resources/images/**/*.*',
-        index: app + 'index.html',
+        client: client,
+        css: temp + 'styles.css',
+        fonts: bower.directory + 'font-awesome/fonts/**/*.*',
+        html: client + '**/*.html',
+        htmltemplates: clientApp + '**/*.html',
+        images: client + 'images/**/*.*',
+        index: client + 'index.html',
+        // app js, with no specs
         js: [
-            app + 'app.js',
-            app + '**/*.js',
-            '!' + app + 'libs/*.*'
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
         ],
-        lib: app + '/lib/**/*.*',
-        resources: './resources/**/*.*',
-        sass: app + 'css/app.scss',
+        jsOrder: [
+            '**/app.module.js',
+            '**/*.module.js',
+            '**/*.js'
+        ],
+        sass: client + 'styles/styles.scss',
+        report: report,
+        root: root,
+        server: server,
+        source: 'src/',
+        stubsjs: [
+            bower.directory + 'angular-mocks/angular-mocks.js',
+            client + 'stubs/**/*.js'
+        ],
         temp: temp,
-        templates: app + 'app/**/*.html',   // all html excluding index
+
+        /**
+         * optimized files
+         */
+        optimized: {
+            app: 'app.js',
+            lib: 'lib.js'
+        },
+
+        /**
+         * plato
+         */
+        plato: {js: clientApp + '**/*.js'},
+
+        /**
+         * browser sync
+         */
+        browserReloadDelay: 1000,
+
+        /**
+         * template cache
+         */
         templateCache: {
             file: 'templates.js',
             options: {
-                module: 'upload',
+                module: 'app.core',
                 root: 'app/',
                 standAlone: false
             }
-        }
+        },
+
+        /**
+         * Bower and NPM files
+         */
+        bower: bower,
+        packages: [
+            './package.json',
+            './bower.json'
+        ],
+
+        /**
+         * specs.html, our HTML spec runner
+         */
+        specRunner: client + specRunnerFile,
+        specRunnerFile: specRunnerFile,
+
+        /**
+         * The sequence of the injections into specs.html:
+         *  1 testlibraries
+         *      mocha setup
+         *  2 bower
+         *  3 js
+         *  4 spechelpers
+         *  5 specs
+         *  6 templates
+         */
+        testlibraries: [
+            nodeModules + '/mocha/mocha.js',
+            nodeModules + '/chai/chai.js',
+            nodeModules + '/mocha-clean/index.js',
+            nodeModules + '/sinon-chai/lib/sinon-chai.js'
+        ],
+        specHelpers: [client + 'test-helpers/*.js'],
+        specs: [clientApp + '**/*.spec.js'],
+        serverIntegrationSpecs: [client + '/tests/server-integration/**/*.spec.js'],
+
+        /**
+         * Node settings
+         */
+        nodeServer: './src/server/app.js',
+        defaultPort: '8001'
     };
 
     /**
@@ -47,13 +131,50 @@ module.exports = function() {
      */
     config.getWiredepDefaultOptions = function() {
         var options = {
-            //bowerJson: config.bower.json,
-            //directory: config.bower.directory,
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
             ignorePath: config.bower.ignorePath,
-            exclude: config.bower.exclude
+            exclude: [
+                /jquery/,
+                '/bootstrap',
+                '/angular-translate'
+            ]
         };
         return options;
     };
 
+    /**
+     * karma settings
+     */
+    config.karma = getKarmaOptions();
+
     return config;
+
+    ////////////////
+
+    function getKarmaOptions() {
+        var options = {
+            files: [].concat(
+                bowerFiles,
+                config.specHelpers,
+                clientApp + '**/*.module.js',
+                clientApp + '**/*.js',
+                temp + config.templateCache.file,
+                config.serverIntegrationSpecs
+            ),
+            exclude: [],
+            coverage: {
+                dir: report + 'coverage',
+                reporters: [
+                    // reporters not supporting the `file` property
+                    {type: 'html', subdir: 'report-html'},
+                    {type: 'lcov', subdir: 'report-lcov'},
+                    {type: 'text-summary'} //, subdir: '.', file: 'text-summary.txt'}
+                ]
+            },
+            preprocessors: {}
+        };
+        options.preprocessors[clientApp + '**/!(*.spec)+(.js)'] = ['coverage'];
+        return options;
+    }
 };
